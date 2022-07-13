744a3b499e911fff5c4f44eddc056fcb0534ed40
valentinak5 <valentinakostic5@gmail.com>
2022-07-13 15:09:28 +0200

    Update db.php

--- functions/db.php
+++ functions/db.php
@@ -1,7 +1,13 @@
 <?php
 
 $con = mysqli_connect("localhost", "root", "", "podeliMisli");
-
+if (!$con) {
+  // whatever processing you want to do upon error in connection
+  // like log error or send an email to admin
+  // I am just printing the error at the moment.
+  echo mysqli_connect_errno() . ":" . mysqli_connect_error();
+  exit;
+}
 function escape($string){
   global $con;
   return mysqli_real_escape_string($con, $string);
